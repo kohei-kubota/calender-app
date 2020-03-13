75db44e372812e26d1c6e950be8182a0c9584f78
kohei.kubota <kohei.kubota@koheikubotanoMacBook-Pro.local>
2020-03-13 11:49:40 +0900

    見た目の変更　ロジックの分離

--- front/src/components/CalenderElement/index.jsx
+++ front/src/components/CalenderElement/index.jsx
@@ -1,21 +1,33 @@
 import React from "react";
 import { Typegraphy, Typography } from "@material-ui/core";
+import dayjs from 'dayjs';
 
 import * as styles from "./style.css";
 import CalenderBoard from "../CalenderBoard";
+import { isSameMonth, isFirstDay, isSameDay } from "../../services/calendar";
 
 const CalenderElement = ({ day }) => {
-  const isFirstDay = day.date() === 1;
-  const format = isFirstDay ? "M月D日" : "D";
+  const today = dayjs();
+
+  const format = isFirstDay(day) ? "M月D日" : "D";
+
+  const isToday = isSameDay(day, today);
+
+  const isCurrentMonth = isSameMonth(day, today);
+  const textColor = isCurrentMonth ? "textPrimary" : "textSecondary";
+
   return (
     <div className={styles.element}>
       <Typography
         className={styles.date}
+        color={textColor}
         align="center"
         variant="caption"
         component="div"
       >
-        {day.format(format)}
+        <span className={isToday ? styles.today : ""}>
+          {day.format(format)}
+        </span>
       </Typography>
     </div>
   );
